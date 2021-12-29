f6b2cbe4b41b1888278d68d236c17e20dee50c82
Stephen McAleese <stephenmcaleese11@gmail.com>
2021-12-29 18:33:19 +0000

    Modified LinkedIn scraping script + added new scraped LinkedIn jobs

--- src/data/linkedin-jobs-scraping-script.py
+++ src/data/linkedin-jobs-scraping-script.py
@@ -1,85 +1,128 @@
 from selenium import webdriver
-import time
-import pandas as pd
-from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import WebDriverException, TimeoutException
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import urllib
+import csv
+import time
+import uuid
 
-# This script opens up a chrome browser
+total_job_posts_scraped = 0
 
-# Dependencies: command to install
-# python3
-# selenium: pip3 install selenium
-# pandas: pip3 install pandas
-# chromedriver: https://www.codegrepper.com/code-examples/python/install+chromedriver+ubuntu+python
+location_data = {
+    'dublin': {
+        'geo_id': '105178154',
+        'long_name': 'Dublin, County Dublin, Ireland'
+    },
+    'ireland': {
+        'geo_id': '104738515',
+        'long_name': 'Ireland'
+    }
+}
 
-def main():
-    # Search:
-    # job: 'Software Engineer', location: 'Dublin, County Dublin, Ireland':
-    url = 'https://www.linkedin.com/jobs/search?keywords=Software%20Engineer&location=Dublin%2C%20County%20Dublin%2C%20Ireland&geoId=105178154&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0'
+def login(browser):
+    elem = browser.find_element_by_id('username')
+    elem.send_keys('job.post.analyzer@gmail.com')
 
-    wd = webdriver.Chrome(executable_path='/usr/bin/chromedriver')
-    wd.get(url)
+    elem = browser.find_element_by_id('password')
+    elem.send_keys('7j)5nQ#/')
+    elem.submit()
 
-    # scroll down the page 3 times to get 100 jobs (each page has 25 jobs):
-    for _ in range(3):
-        wd.execute_script('window.scrollTo(0, document.body.scrollHeight)')
-        time.sleep(3)
+def get_total_num_pages(browser):
+    try:
+        pagination_section = browser.find_element_by_css_selector('section.jobs-search-two-pane__pagination')
+        list_elements = pagination_section.find_elements_by_tag_name('li')
+        total_num_pages = int(list_elements[-1].text)
+        return total_num_pages
+    except TimeoutException:
+        print('failed to find total number of pages')
+        return 1
 
-    # Now get all the jobs
-    jobs_list = wd.find_element_by_class_name('jobs-search__results-list')
-    jobs = jobs_list.find_elements_by_tag_name('li')
-
-    print('Number of jobs:', len(jobs))
-
-    # Get the data from each job:
-    job_titles = []
-    company_names = []
-    dates = []
-    descriptions = []
-
-    for job in jobs:
-        # simple information from job list (on left side of page)
-        title = job.find_element_by_class_name('base-search-card__title').get_attribute('innerText')
-        company = job.find_element_by_class_name('hidden-nested-link').get_attribute('innerText')
-        date = job.find_element_by_css_selector('div.base-search-card__metadata>time').get_attribute('datetime')
-        job_titles.append(title)
-        company_names.append(company)
-        dates.append(date)
-
+def scrape_page(jobs_on_page, browser, csvwriter):
+    global total_job_posts_scraped
+    for job in jobs_on_page:
+        # one second delay to prevent too many requests error (HTTP code 429)
+        time.sleep(1)
         try:
             job.click()
-        except WebDriverException:
-            descriptions.append('no description found')
+        except WebDriverException as error:
+            print('click failed, skipping job post:', error)
+            continue
+        try:
+            company_name = job.find_element_by_css_selector('a.job-card-container__company-name').get_attribute('innerText')
+            print('company_name:', company_name)
+
+            job_title = job.find_element_by_class_name('job-card-list__title').get_attribute('innerText')
+            print('job_title:', job_title)
+
+            location = job.find_element_by_css_selector('ul.job-card-container__metadata-wrapper').get_attribute('innerText').replace('\n', ' ')
+            print('location:', location)
+
+            description = browser.find_element_by_css_selector('div.jobs-description-content__text > span').get_attribute('innerText')
+
+            data = [uuid.uuid4().hex, company_name, job_title, location, description]
+            csvwriter.writerow(data)
+            total_job_posts_scraped += 1
+            print('Total job posts scraped:', total_job_posts_scraped, end='\n\n')
+        except Exception as error:
+            print('failed to process job post, skipping:', error)
+            time.sleep(1)
             continue
 
-        # if there is still no description after 3 seconds, add 'No description found' and move on
-        try:
-            wait  = WebDriverWait(wd, 2)
-            wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'div.show-more-less-html__markup')))
-            description = wd.find_element_by_css_selector('div.show-more-less-html__markup').get_attribute('innerText')
-            print(f'Added new job description for {title}')
-            descriptions.append(description)
-        except TimeoutException:
-            print('No description found')
-            descriptions.append('No description found')
+def main():
+    browser = webdriver.Chrome(executable_path='/usr/bin/chromedriver')
+    browser.implicitly_wait(10)
 
-    # print(job_titles)
-    # print(company_names)
-    # print(dates)
+    login_link = 'https://www.linkedin.com/checkpoint/rm/sign-in-another-account?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin'
+    browser.get(login_link)
+    login(browser)
 
-    print('length of descriptions:', len(descriptions))
+    search_job_title = 'software engineer'
+    search_location = 'dublin'
 
-    processed_data = pd.DataFrame({
-        'Company Name': company_names,
-        'Job Title': job_titles,
-        'Date': dates,
-        'Description': descriptions,
-    })
+    url_root = 'https://www.linkedin.com/jobs/search?geoId='
+    job_posts_url = urllib.parse.quote(url_root
+                             + location_data[search_location]['geo_id']
+                             + '&keywords=' + search_job_title 
+                             + '&location=' + location_data[search_location]['long_name'],
+                             safe='/:?=&')
 
-    csv = processed_data.to_csv('scraped-data.csv')
+    browser.get(job_posts_url)
+    time.sleep(3) # wait for the jobs page to load
+
+    csvfile = open('linkedin-scraped-data.csv', 'w')
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(['ID', 'Company', 'Job Title', 'Location', 'Description'])
+
+    num_pages = get_total_num_pages(browser)
+    print('num pages:', num_pages)
+
+    consecutive_error_count = 0
+    for page_num in range(1, num_pages + 1):
+        jobs_on_page = browser.find_elements_by_css_selector('li.jobs-search-results__list-item')
+        time.sleep(2)
+
+        scrape_page(jobs_on_page, browser, csvwriter)
+
+        if page_num < num_pages:
+            try:
+                next_page_element = browser.find_element_by_xpath('//li/button[@aria-label="Page {}"]'.format(page_num + 1))
+                next_page_element.click()
+                print('clicked page {}'.format(page_num + 1))
+                consecutive_error_count = 0
+            except Exception as error:
+                print('failed to find or click next page link:', error)
+                consecutive_error_count += 1
+                if consecutive_error_count > 2:
+                    print('too many errors, script exiting...')
+                    break
+                else:
+                    print('error, skipping page...')
+                    continue
+
+    csvfile.close()
 
 if __name__ == '__main__':
     main()

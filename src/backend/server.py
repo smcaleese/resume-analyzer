abd65e6ab2ce566685e2284840e1c943b5a99c4c
Stephen McAleese <stephenmcaleese11@gmail.com>
2021-12-30 09:56:21 +0000

    Added server test, Bulma and Tailwind CSS libraries, updated README

--- src/backend/server.py
+++ src/backend/server.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/status")
 def status():
-    return "Server is running"
+    return {"message": "Server is running"}
--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from 'react'
+import './App.scss'
 
 function App() {
+  const testServer = () => {
+    fetch('http://localhost:8000/status')
+      .then(response => response.json())
+      .then(json => alert(json.message))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App center">
+      <div className="box main-box">
+        <div className="box-content center">
+          <p className="font-bold">Click this button to test the server</p>
+          <button className="button is-primary" onClick={testServer}>button</button>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

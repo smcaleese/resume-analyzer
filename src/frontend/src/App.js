baac1b0153899942ac020b9b3ce07cb83c7b49d2
Darragh McGonigle <darragh.mcgonigle3@mail.dcu.ie>
2022-01-09 15:11:37 +0000

    Added file drag and drop to frontend

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,6 +1,7 @@
-import {useEffect, useState} from 'react'
+import { useState, useCallback} from 'react'
 import styled from 'styled-components'
 import classnames from 'classnames'
+import {useDropzone} from "react-dropzone";
 
 /*
 DONE:
@@ -28,16 +29,25 @@ const postResume = async (file) => {
 const App = ({className}) => {
   const [file, setFile] = useState('')
 
-  const handleFileUpload = (e) => {
-    const file = e.target.files[0]
+  const checkFileValidity = (file) => {
     const notPDF = file.type !== 'application/pdf'
     const fileGreaterThan5MB = file.size >= 5000000
-
     if(notPDF) {
       alert('File must have the PDF format')
+      return false
     } else if(fileGreaterThan5MB) {
       alert('File max size is 5MB')
+      return false
     } else {
+      console.log('File is valid')
+      return true
+    }
+  }
+
+  const handleFileUpload = (e) => {
+    const file = e.target.files[0]
+
+    if(checkFileValidity(file)) {
       console.log('upload successful')
       setFile(file)
     }
@@ -49,10 +59,23 @@ const App = ({className}) => {
     postResume(file)
   }
 
+  const onDrop = useCallback((acceptedFile, rejectedFile) => {
+    const file = acceptedFile[0]
+
+    if(checkFileValidity(file)) {
+      console.log('upload successful')
+      setFile(file)
+    }
+  }, [])
+  const {getRootProps, getInputProps} = useDropzone({ onDrop });
   return (
     <div className={classnames(className, 'App', 'center')}>
       <form className='box upload-form center'>
         <h1 className='title'>Upload a resume</h1>
+        <div {...getRootProps()} className='drag-drop-box'>
+          <input {...getInputProps()} />
+          <p className='drag-drop-box-text'>{file ? file.name : "Please Upload file" }</p>
+        </div>
         <div className='upload-button'>
           <label htmlFor='file-upload' className='file-upload-label button is-primary'>
             File Upload (PDF)
@@ -68,10 +91,12 @@ const App = ({className}) => {
 export default styled(App)`
   height: 100vh;
   width: 100vw;
+  position:relative;
 
   .upload-form {
     width: 50rem;
     padding: 2rem;
+    z-index: 100;
 
     .upload-button {
       .file-upload-label {
@@ -84,9 +109,24 @@ export default styled(App)`
         display: none;
       }
     }
-
+    
     button {
       width: 10rem;
+    }
+
+    .drag-drop-box {
+      height:400px;
+      width:100%;
+      border-radius: 20px;
+      border: 4px dashed grey;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      
+      .drag-drop-box-text{
+        font-size:1.2rem;
+        font-weight:bold;
+      }
     }
 
     .submit-button {

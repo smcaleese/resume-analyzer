f817c6bf16c3a6902ee9e079bdb8f5955b9c368d
Darragh McGonigle <darragh.mcgonigle3@mail.dcu.ie>
2022-01-22 13:37:04 +0000

    Fixed Z-Index issue on header bar

--- src/frontend/src/components/Header.js
+++ src/frontend/src/components/Header.js
@@ -19,21 +19,13 @@ const Header = ({ className }) => {
                 </Navbar.Brand>
             </Container>
         </Navbar>
-        // <div className={className}>
-        // <nav className='navbar' role="navigation" aria-label="main navigation">
-        //     <div className="navbar-brand">
-        //         <div className="navbar-item" style={{padding:0, lineHeight:2, fontSize:"3em"}}>
-        //             <img src={logo} alt="Logo" width="100" height="100"  />
-        //             <span>ResumAI</span>
-        //         </div>
-        //     </div>
-        // </nav>
-        // </div>
     )
 }
 
 export default styled(Header)`
 box-shadow: 0px 2px 5px #ccc;
+background-color: #fff;
+z-index: 3000;
 
 .product-name{
     line-height: 100px; 

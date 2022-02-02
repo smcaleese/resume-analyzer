d8330533ada7c0bcef3975c4338c558c5b65cc11
Stephen McAleese <stephenmcaleese11@gmail.com>
2022-02-02 23:31:53 +0000

    updated Heroku PostgreSQL URL, changed CORS settings

--- src/backend/server.py
+++ src/backend/server.py
@@ -16,7 +16,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=['http://localhost:3000', 'https://fourth-year-project-front-end.herokuapp.com'],
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],

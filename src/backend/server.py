faed615b51674983b0367bf3dfacc399831da2f7
Stephen McAleese <stephenmcaleese11@gmail.com>
2022-02-02 22:36:46 +0000

    updated Procfile, changed CORS to allow any origin

--- src/backend/server.py
+++ src/backend/server.py
@@ -16,7 +16,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['http://localhost:3000'],
+    allow_origins=['*'],
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],

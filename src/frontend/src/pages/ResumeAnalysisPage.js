4afbaf03a130b23e7c0b2c48844e3476632eac99
Stephen McAleese <stephenmcaleese11@gmail.com>
2022-03-20 19:08:06 +0000

    Added react-grid-layout and new card grid

--- src/frontend/src/pages/ResumeAnalysisPage.js
+++ src/frontend/src/pages/ResumeAnalysisPage.js
@@ -9,6 +9,9 @@ import YearsOfExperiencePanel from '../components/report-components/YearsOfExper
 import { Container, Col, Row, Card } from 'react-bootstrap'
 import classnames from 'classnames'
 import { AppContext } from '../App'
+import { Responsive, WidthProvider } from 'react-grid-layout'
+
+const ResponsiveGridLayout = WidthProvider(Responsive)
 
 const NoResumePage = ({ className }) => (
     <div className={classnames(className, 'center')}>
@@ -43,36 +46,58 @@ const FileDisplayPage = ({ className }) => {
         return <StyledNoResumePage />
     }
 
+    const layouts = {
+        lg: [
+            {i: 'skills', x: 0, y: 0, w: 4, h: 18},
+            {i: 'skill-frequencies', x: 4, y: 0, w: 4, h: 32},
+            {i: 'pdf-viewer', x: 8, y: 0, w: 4, h: 40},
+            {i: 'matching-jobs', x: 0, y: 2, w: 8, h: 25},
+        ],
+        md: [
+            {i: 'skills', x: 0, y: 0, w: 3, h: 15},
+            {i: 'skill-frequencies', x: 3, y: 0, w: 3, h: 32},
+            {i: 'pdf-viewer', x: 3, y: 2, w: 3, h: 44},
+            {i: 'matching-jobs', x: 0, y: 2, w: 3, h: 26},
+        ],
+        sm: [
+            {i: 'skills', x: 0, y: 0, w: 2, h: 12},
+            {i: 'skill-frequencies', x: 0, y: 1, w: 2, h: 32},
+            {i: 'pdf-viewer', x: 0, y: 2, w: 2, h: 50},
+            {i: 'matching-jobs', x: 0, y: 6, w: 2, h: 26},
+        ]
+    }
+
     return (
-        <div className={className}>
-            <Container fluid>
-                <Row>
-                    <Col xl={8}>
-                        <Row className='left-row'>
-                            <Col lg={6}>
-                                <Row>
-                                    <SkillsDisplayPanel />
-                                </Row>
-                            </Col>
-                            <Col lg={6} className='skillFreqPanel'>
-                                <SkillFrequenciesPanel />
-                            </Col>
-                        </Row>
-                        <Row>
-                            <JobsDisplayPanel />
-                        </Row>
-                    </Col>
-                    <Col xl={4}>
-                        <PDFPageViewer />
-                    </Col>
-                </Row>
-            </Container>
-        </div>
+        <ResponsiveGridLayout
+            className={className}
+            layouts={layouts}
+            rowHeight={10}
+            breakpoints={{ lg: 1200, md: 1000, sm: 800 }}
+            cols={{ lg: 12, md: 6, sm: 2 }}
+            style={{ height: '850px' }}
+        >
+            <div key='skills'>
+                <SkillsDisplayPanel />
+            </div>
+            <div key='skill-frequencies'>
+                <SkillFrequenciesPanel />
+            </div>
+            <div key='pdf-viewer'>
+                <PDFPageViewer />
+            </div>
+            <div key='matching-jobs'>
+                <JobsDisplayPanel />
+            </div>
+        </ResponsiveGridLayout>
     )
 }
 
 const StyledFileDisplayPage = styled(FileDisplayPage)`
     margin: 0px 10px 0px 110px;
+
+    .react-grid-layout {
+        margin: 0;
+    }
 
     .skillFreqPanel {
         padding-right: 0;

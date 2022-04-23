a3c708dcc7d4a2aed9f5d04317b7b0f0c38998dd
Darragh McGonigle <darragh.mcgonigle3@mail.dcu.ie>
2022-04-23 14:17:32 +0100

    Changed dropdown button colour

--- src/frontend/src/components/RoleDropdown.js
+++ src/frontend/src/components/RoleDropdown.js
@@ -33,6 +33,13 @@ export default styled(RoleDropdown)`
         padding: 0.4rem;
         font-size: 1.1rem;
         box-shadow: none !important;
+        background-color: #31DAC1;
+        border: 2px #31DAC1 solid;
+    }
+    .btn-check:active+.btn-success, .btn-check:checked+.btn-success, .btn-success.active, .btn-success:active, .show>.btn-success.dropdown-toggle {
+        color: #fff;
+        background-color: #1c8071;
+        border-color: #13653f;
     }
 
 `

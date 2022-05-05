7efa4a732ed1261c8f82471b2c469c6f75a72042
Stephen McAleese <stephenmcaleese11@gmail.com>
2022-05-05 14:03:55 +0100

    Updated db config

--- src/backend/dbconfig.py
+++ src/backend/dbconfig.py
@@ -9,14 +9,25 @@ to be created. Comment out a setting to change which database is used by the pop
 # needed for unit tests:
 env_local = {
     'username': 'postgres',
-    'password': 'postgres',
+    'password': 'password',
     'host': 'localhost',
     'port': '5432',
     'database': 'postgres',
 }
 
+if 'RDS_HOSTNAME' in os.environ:
+    aws_config = {
+        'username': os.environ['RDS_USERNAME'],
+        'password': os.environ['RDS_PASSWORD'],
+        'host': os.environ['RDS_HOSTNAME'],
+        'port': os.environ['RDS_PORT'],
+        'database': os.environ['RDS_DB_NAME'],
+    }
+else:
+    aws_config = {}
+
 config = {
     **env_local,
     **dotenv_values('.env.production'),
-    **os.environ,  # AWS production settings
+    **aws_config,
 }
